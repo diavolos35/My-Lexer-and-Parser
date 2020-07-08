959ec206f6aab52e5c6c855b39bddf3885c1475e
Baran Gezenoglu <43960222+diavolos35@users.noreply.github.com>
2020-07-08 20:38:04 +0300

    Add files via upload

---
+++ src/cmake-build-debug/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/lexer.c.obj"
+  "CMakeFiles/src.dir/parser.c.obj"
+  "libsrc.dll.a"
+  "src.exe"
+  "src.exe.manifest"
+  "src.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
